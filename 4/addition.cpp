86b216cf073ed9c069f91de1054d961f31e64fdf
seojin040325 <seojin040325@gmail.com>
2026-03-27 18:51:49 +0900

    Update addition.cpp

--- 4/addition.cpp
+++ 4/addition.cpp
@@ -6,9 +6,8 @@ int uadd_ok(unsigned a, unsigned b)
 	int sum;
 	sum = a + b;
 
-	if (sum < a){
-	return 0;
-	}
+	if (sum < a)
+		return 0;
 	return 1;
 }
 
@@ -18,9 +17,9 @@ int tadd_ok(int a, int b)
 	sum = a + b;
 
 	if (a > 0 && b > 0 && sum < 0)
-	return 0;
+		return 0;
 	else if (a < 0 && b < 0 && sum > 0)
-	return 0;
+		return 0;
 	return 1;
 }
 
@@ -31,8 +30,8 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-	cout << "You should insert two numbers in a hexadecimal way" << endl;
-	return 0;
+		cout << "You should insert two numbers in a hexadecimal way" << endl;
+		return 0;
 	}
 	a = strtol(argv[1], NULL, 16);
 	b = strtol(argv[2], NULL, 16);
@@ -40,11 +39,15 @@ int main(int argc, char *argv[])
 	y = (unsigned) b;
 
 	cout << "Unsigned Addition ===" << endl;
-	if (uadd_ok(x, y))																				cout << "No overflow" << endl;																			else
-	cout << "Overflow" << endl;	
-	cout << "Signed Addition ===" << endl;																									if (tadd_ok(a, b))
-	cout << "No overflow" << endl;
-																																else
-																																			cout << "Overflow" << endl;
-																																				return 0;
+	if (uadd_ok(x, y))
+		cout << "No overflow" << endl;
+	else
+		cout << "Overflow" << endl;
+	
+	cout << "Signed Addition ===" << endl;
+	if (tadd_ok(a, b))
+		cout << "No overflow" << endl;
+	else
+		cout << "Overflow" << endl;
+	return 0;
 }
