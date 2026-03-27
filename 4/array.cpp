b50f0446d87d902aa530fede6de795ff41b0d7e1
seojin040325 <seojin040325@gmail.com>
2026-03-27 18:52:13 +0900

    Update array.cpp

--- 4/array.cpp
+++ 4/array.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main(int argc, char *argv[])
 {
-		int arr[10];
-			int i;
+	int arr[10];
+	int i;
 
-				for (i=0; i<10; i++)
-							arr[i] = 10-i;
+	for (i=0; i<10; i++)
+		arr[i] = 10-i;
 
-								cout << "arr[0] = " << arr[0] << endl;
-									cout << "arr[3] = " << arr[3] << endl;
-										cout << "arr[7] = " << arr[7] << endl;
+	cout << "arr[0] = " << arr[0] << endl;
+	cout << "arr[3] = " << arr[3] << endl;
+	cout << "arr[7] = " << arr[7] << endl;
 
-											return 0;
+	return 0;
 }
